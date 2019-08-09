b3bf9cd52c6a5701cac01883167bacf91996c686
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2019-08-09 12:11:55 +0530

    Update Appscan.bat

--- Appscan.bat
+++ Appscan.bat
@@ -1,5 +1,5 @@
 echo "logging in"
-login_file https://blrkeccdsrv01.ad.infosys.com:9443/ase " C:\Users\Kritika_S\.ounce\ouncecli.token"  -acceptssl
+login_file https://blrkeccdsrv01.ad.infosys.com:9443/ase "C:\Users\Kritika_S\.ounce\ouncecli.token"  -acceptssl
 echo "logged in"
 oa "C:\Program Files (x86)\Jenkins\workspace\Cicd_pipeline\AppScan\AppScan_Pipeline.paf"
 echo "completed"
