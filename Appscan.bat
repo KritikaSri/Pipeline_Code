df411adf3c8bc47123f348c28a7d0ceaadef3b05
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2019-08-09 11:05:43 +0530

    Add files via upload

---
+++ Appscan.bat
@@ -0,0 +1,7 @@
+echo "logging in"
+login_file https://blrkeccdsrv01.ad.infosys.com:9443/ase " C:\Users\Kritika_S\.ounce\ouncecli.token"  -acceptssl
+echo "logged in"
+oa "C:\Program Files (x86)\Jenkins\workspace\Cicd_pipeline\AppScan\AppScan_Pipeline.paf"
+echo "completed"
+scan "C:\Program Files (x86)\Jenkins\workspace\Cicd_pipeline\AppScan\AppScan_Pipeline.ppf"
+pause 
